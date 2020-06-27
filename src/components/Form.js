964beb8b654b5652dbd7aef44ea488d9bfb7fa06
gowthamaraj <gowthamaraj360@gmail.com>
2020-06-27 13:50:12 +0530

    edited: form.js

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,7 +4,7 @@ const Form = props => (
     <form onSubmit={props.getRecipes} style={{ marginBottom:"2rem" }} className="form-inline d-flex justify-content-center sticky-top">
       <div className="form-group">
         <input className="form-control" type="text" name="recipeName" />
-        <button type="submit" className="btn btn-primary ml-2">Search</button>
+        <button type="submit" className="btn btn-primary ml-2 my-2">Search</button>
       </div>
     </form>
   );
